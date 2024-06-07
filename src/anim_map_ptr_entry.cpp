7b25e4b12d8046fe5e286aa9ab3d6e74eaf94b37
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-07 20:52:19 +0300

    Refactored anim_map_ptr_entry

--- src/anim_map_ptr_entry.cpp
+++ src/anim_map_ptr_entry.cpp
@@ -3,3 +3,11 @@
 #include "common.h"
 
 VALIDATE_SIZE(anim_map_ptr_entry, 0x10);
+
+void anim_map_ptr_entry::un_mash(
+        generic_mash_header *a2,
+        void *,
+        generic_mash_data_ptrs *a4)
+{
+    this->field_8.custom_un_mash(a2, &this->field_8, a4, nullptr);
+}
