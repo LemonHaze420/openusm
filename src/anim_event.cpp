7da9dce176cdd7437b127e3f76ab8081d6589036
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-09 16:19:44 +0300

    Implemented anim_event

---
+++ src/anim_event.cpp
@@ -0,0 +1,5 @@
+#include "anim_event.h"
+
+#include "common.h"
+
+VALIDATE_SIZE(anim_event, 0x18);
---
+++ src/anim_event.h
@@ -0,0 +1,7 @@
+#include "event.h"
+
+struct anim_event : event {
+    string_hash field_C;
+    void *field_10;
+    int my_num_params;
+};
