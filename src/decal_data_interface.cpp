de82f72f95a260e314ec5d2fddcedc07e5cae306
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-09 16:27:03 +0300

    Refactored decal_data_interface

--- src/decal_data_interface.cpp
+++ src/decal_data_interface.cpp
@@ -1,20 +1,70 @@
 #include "decal_data_interface.h"
 
+#include "anim_event.h"
 #include "common.h"
+#include "conglom.h"
+#include "entity_base_vhandle.h"
+#include "event.h"
+#include "event_manager.h"
 #include "func_wrapper.h"
+#include "memory.h"
 
 VALIDATE_SIZE(decal_data_interface, 0x80);
 
-decal_data_interface::decal_data_interface()
+decal_data_interface::decal_data_interface(
+        conglomerate *a2) : conglomerate_interface(a2)
 {
+    this->m_vtbl = 0x00883DD4;
 
+    this->field_C = false;
+    this->field_D = false;
+
+    this->field_10 = a2;
+    this->constructor_common();
 }
 
 bool decal_data_interface::is_dynamic() const
 {
-    return this->field_8;
+    return this->dynamic;
 }
 
 void decal_data_interface::frame_advance_all_decal_interfaces(Float a1) {
     CDECL_CALL(0x004D1CC0, a1);
+}
+
+void decal_data_interface::add_to_decal_ifc_list()
+{
+    if ( all_decal_interfaces == nullptr )
+    {
+        auto *mem = mem_alloc(sizeof(_std::vector<decal_data_interface *>));
+        all_decal_interfaces = new (mem) _std::vector<decal_data_interface *> {};
+    }
+
+    all_decal_interfaces->push_back(this);
+}
+
+void terrain_fx_callback(event *the_event, entity_base_vhandle , void *a3)
+{
+    assert(the_event != nullptr);
+
+    auto v3 = bit_cast<anim_event *>(the_event)->field_C;
+
+    void (__fastcall *sub_509380)(void *, void *edx, string_hash *) = CAST(sub_509380, 0x00509380);
+    sub_509380(a3, nullptr, &v3);
+}
+
+void decal_data_interface::constructor_common()
+{
+    this->field_14 = nullptr;
+    this->field_C = true;
+    if ( this->field_D )
+    {
+        auto *mem = mem_alloc(sizeof(_std::vector<entity *>));
+        this->field_14 = new (mem) _std::vector<entity *> {};
+
+        auto v6 = this->my_conglomerate->get_my_handle();
+        this->field_18 = event_manager::add_callback(event::TERRAIN_FX, v6, terrain_fx_callback, this->field_14, false);
+    }
+
+    this->add_to_decal_ifc_list();
 }
--- src/decal_data_interface.h
+++ src/decal_data_interface.h
@@ -1,19 +1,41 @@
 #pragma once
 
-#include "generic_interface.h"
+#include "conglomerate_interface.h"
+
 #include "float.hpp"
+#include "mashable_vector.h"
+#include "variable.h"
+
+#include <vector.hpp>
 
 struct conglomerate;
+struct entity;
+struct terrain_decal;
 
-struct decal_data_interface : generic_interface {
-    conglomerate *field_4;
-    bool field_8;
-    char field_C[0x74];
+struct decal_data_interface : conglomerate_interface {
+    bool field_C;
+    bool field_D;
+    conglomerate *field_10;
+    _std::vector<entity *> *field_14;
+    int field_18;
+    int field_1C;
+    mashable_vector<int> field_20;
+    mashable_vector<int> field_28;
+    mashable_vector<float> field_30;
+    mashable_vector<terrain_decal> field_38[9];
+    int field_80;
 
-    decal_data_interface();
+    decal_data_interface(conglomerate *a2);
 
     bool is_dynamic() const;
 
+    void add_to_decal_ifc_list();
+
     //0x004D1CC0
     static void frame_advance_all_decal_interfaces(Float a1);
+
+    //virtual
+    void constructor_common();
+
+    static inline auto & all_decal_interfaces = var<_std::vector<decal_data_interface *> *>(0x01564534);
 };
