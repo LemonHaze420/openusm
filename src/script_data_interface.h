6fd64e63dd47eb197636bc0c04aedf95d49b5d71
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-09 16:27:39 +0300

    Refactored script_data_interface

--- src/script_data_interface.h
+++ src/script_data_interface.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "generic_interface.h"
+#include "conglomerate_interface.h"
 
-struct script_data_interface : generic_interface
+struct script_data_interface : conglomerate_interface
 {
-    char field_0[0x88];
+    char field_0[0x80];
 
     void _un_mash(generic_mash_header *a2, void *a3, void *a4, generic_mash_data_ptrs *a5);
 };
