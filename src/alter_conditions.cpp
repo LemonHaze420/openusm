6065561bd2602fe1be95f63f4d31b3ed534e0edf
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 13:16:05 +0300

    Implemented construction of alter_conditions from raw memory.

--- src/alter_conditions.cpp
+++ src/alter_conditions.cpp
@@ -7,6 +7,10 @@ namespace als
 {
     VALIDATE_SIZE(alter_conditions, 0xC);
 
+    alter_conditions::alter_conditions(from_mash_in_place_constructor *a2) : field_8(a2)
+    {
+    }
+
     void alter_conditions::unmash(mash_info_struct *a1, void *)
     {
         a1->unmash_class_in_place(this->field_8, this);
--- src/alter_conditions.h
+++ src/alter_conditions.h
@@ -2,6 +2,7 @@
 
 #include "string_hash.h"
 
+struct from_mash_in_place_constructor;
 struct mash_info_struct;
 
 namespace als
@@ -11,6 +12,8 @@ namespace als
         int field_0;
         int field_4;
         string_hash field_8;
+
+        alter_conditions(from_mash_in_place_constructor *a2);
 
         void unmash(mash_info_struct *a1, void *a3);
     };
