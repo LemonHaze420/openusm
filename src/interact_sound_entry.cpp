10e6f50c2c93d3a6205663f4721c00771d71e638
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 12:33:25 +0300

    Implemented construction of interact_sound_entry from raw memory.

--- src/interact_sound_entry.cpp
+++ src/interact_sound_entry.cpp
@@ -5,6 +5,24 @@
 
 VALIDATE_SIZE(interact_sound_entry, 0x14);
 
+interact_sound_entry::interact_sound_entry(from_mash_in_place_constructor *a1) : field_0(a1)
+{
+    this->initialize(mash::FROM_MASH);
+}
+
+void interact_sound_entry::initialize(mash::allocation_scope a2)
+{
+    if ( a2 == mash::ALLOCATED )
+    {
+        this->field_0 = {0};
+        this->field_4 = 0;
+        this->field_8 = 0;
+        this->field_C = 3.4028235e38;
+        this->field_10 = true;
+        this->field_11 = true;
+    }
+}
+
 void interact_sound_entry::unmash(mash_info_struct *a1, void *)
 {
     a1->unmash_class_in_place(this->field_0, this);
--- src/interact_sound_entry.h
+++ src/interact_sound_entry.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "mash.h"
 #include "string_hash.h"
 
+struct from_mash_in_place_constructor;
 struct mash_info_struct;
 
 struct interact_sound_entry
@@ -9,8 +11,13 @@ struct interact_sound_entry
     string_hash field_0;
     int field_4;
     int field_8;
-    int field_C;
-    int field_10;
+    float field_C;
+    bool field_10;
+    bool field_11;
+
+    interact_sound_entry(from_mash_in_place_constructor *a1);
+
+    void initialize(mash::allocation_scope a2);
 
     void unmash(mash_info_struct *a1, void *);
 };
