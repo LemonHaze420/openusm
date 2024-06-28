d6f99dd3bad9a7c5402c1e8513e652e54daaec0e
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 13:47:22 +0300

    Implemented tracking_panel.

---
+++ src/tracking_panel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "mstring.h"
+#include "resource_key.h"
+
+struct from_mash_in_place_constructor;
+
+struct tracking_panel {
+
+    tracking_panel(from_mash_in_place_constructor *a2);
+};
