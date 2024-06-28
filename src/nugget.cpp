74cd2c481e1e9c18eca363c49a39e937bfe6cc24
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 13:49:42 +0300

    Implemented nugget.

---
+++ src/nugget.cpp
@@ -0,0 +1,7 @@
+#include "nugget.h"
+
+#include "common.h"
+
+nugget::nugget() : field_4(false) {
+    this->m_vtbl = 0x00873708;
+}
---
+++ src/nugget.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "mash_virtual_base.h"
+
+struct nugget : mash_virtual_base {
+    bool field_4;
+
+    nugget();
+};
