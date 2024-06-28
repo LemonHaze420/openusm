162aff9056c99796cb7c659521ce49781c63ab81
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 13:02:42 +0300

    Implemented constructors for force_transitions.

--- src/force_transitions.cpp
+++ src/force_transitions.cpp
@@ -1,11 +1,18 @@
 #include "force_transitions.h"
 
+#include "alter_conditions.h"
 #include "common.h"
 #include "mash_info_struct.h"
 
 namespace als
 {
     VALIDATE_SIZE(force_transitions, 0x18);
+
+    force_transitions::force_transitions() : field_0(), field_4() {}
+
+    force_transitions::force_transitions(from_mash_in_place_constructor *a2) : field_0(a2), field_4(a2)
+    {
+    }
 
     void force_transitions::unmash(mash_info_struct *a1, void *)
     {
--- src/force_transitions.h
+++ src/force_transitions.h
@@ -3,6 +3,8 @@
 #include "string_hash.h"
 #include "mvector.h"
 
+struct from_mash_in_place_constructor;
+
 namespace als
 {
     struct alter_conditions;
@@ -11,6 +13,10 @@ namespace als
     {
         string_hash field_0;
         mVector<als::alter_conditions> field_4;
+
+        force_transitions();
+
+        force_transitions(from_mash_in_place_constructor *a2);
 
         void unmash(mash_info_struct *a1, void *);
     };
