42a7d10c8c664f03200582ae193e56752ceadb6a
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 12:55:06 +0300

    Implemented construction of combo_system_weapon from raw memory.

--- src/combo_system_weapon.h
+++ src/combo_system_weapon.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "mash.h"
 #include "resource_key.h"
 
+struct from_mash_in_place_constructor;
 struct mash_info_struct;
 
 struct combo_system_weapon {
@@ -10,9 +12,12 @@ struct combo_system_weapon {
     resource_key field_10;
     string_hash field_18;
     string_hash field_1C;
-    int field_20;
+    int8_t field_20;
+    bool field_21;
 
-    combo_system_weapon();
+    combo_system_weapon(from_mash_in_place_constructor *a2);
+
+    void initialize(mash::allocation_scope a2);
 
     void unmash(mash_info_struct *a1, void *a3);
 };
