cb324173a791ceccb329094286363ff5841de97c
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 12:31:05 +0300

    Implemented construction of ai_adv_strength_test_data from raw memory.

--- src/ai_adv_strength_test_data.h
+++ src/ai_adv_strength_test_data.h
@@ -1,5 +1,6 @@
 #pragma once
 
+struct from_mash_in_place_constructor;
 struct mash_info_struct;
 
 struct ai_adv_strength_test_data
@@ -11,6 +12,8 @@ struct ai_adv_strength_test_data
     int field_10;
     int field_14;
     int field_18;
+
+    ai_adv_strength_test_data(from_mash_in_place_constructor *) {}
 
     void unmash(mash_info_struct *a1, void *a3);
 };
