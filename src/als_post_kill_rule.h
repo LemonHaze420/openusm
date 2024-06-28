43bea1d6f933167fa582a4632d642c53531f1a1f
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 13:25:29 +0300

    Implemented construction of als::post_kill_rule from raw memory.

--- src/als_post_kill_rule.h
+++ src/als_post_kill_rule.h
@@ -2,12 +2,16 @@
 
 struct mash_info_struct;
 
+struct from_mash_in_place_constructor;
+
 namespace als
 {
     struct post_kill_rule
     {
         int field_0;
         int field_4;
+
+        post_kill_rule(from_mash_in_place_constructor *) {}
 
         void unmash(mash_info_struct *, void *);
     };
