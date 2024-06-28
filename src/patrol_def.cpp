f2916feb8e6baa55672fcef343c99f31e7848181
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 13:45:08 +0300

    Implemented construction of patrol_def from raw memory.

--- src/patrol_def.cpp
+++ src/patrol_def.cpp
@@ -3,3 +3,43 @@
 #include "common.h"
 
 VALIDATE_SIZE(patrol_def, 0x28u);
+
+enum {
+    PATROL_SKILL_FILTER_EASY = 0,
+    PATROL_SKILL_FILTER_MEDIUM = 1,
+    PATROL_SKILL_FILTER_HARD = 2,
+};
+
+patrol_def::patrol_def(from_mash_in_place_constructor *a2) : field_0(a2)
+{
+    this->initialize(mash::FROM_MASH);
+}
+
+void patrol_def::initialize(mash::allocation_scope a2)
+{
+    if ( a2 )
+    {
+        assert(neighborhood_id >= 0);
+        
+        assert(skill_filter == PATROL_SKILL_FILTER_EASY || skill_filter == PATROL_SKILL_FILTER_MEDIUM || skill_filter == PATROL_SKILL_FILTER_HARD);
+
+        assert(num_nodes >= 0 && num_nodes < 10000);
+
+        assert(script_difficulty >= 0 && script_difficulty < 10000);
+    }
+    else
+    {
+        this->clear();
+    }
+}
+
+void patrol_def::clear()
+{
+    this->field_4 = vector3d {0.0, 0.0, 0.0};
+    this->neighborhood_id = 0;
+    this->skill_filter = 2;
+    this->num_nodes = 5;
+    this->script_difficulty = 0;
+    this->field_14 = 5;
+    this->field_24 = 0;
+}
--- src/patrol_def.h
+++ src/patrol_def.h
@@ -1,18 +1,26 @@
 #pragma once
 
+#include "mash.h"
 #include "string_hash.h"
+#include "vector3d.h"
+
+struct from_mash_in_place_constructor;
 
 struct patrol_def {
     string_hash field_0;
-    int field_4;
-    int field_8;
-    int field_C;
-    int field_10;
+    vector3d field_4;
+    int neighborhood_id;
     int field_14;
-    int field_18;
-    int field_1C;
-    int field_20;
+    int num_nodes;
+    int skill_filter;
+    int script_difficulty;
     int field_24;
+
+    patrol_def(from_mash_in_place_constructor *a2);
+
+    void initialize(mash::allocation_scope a2);
+
+    void clear();
 
     string_hash get_id_hash() const {
         return this->field_0;
