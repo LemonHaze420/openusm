4b850635ad48a9fc8718af1596335e4dd21e7733
MrMartinIden <MrMartinIden@protonmail.com>
2024-06-28 13:51:17 +0300

    Implemented entity_viseme_entry.

---
+++ src/entity_viseme_entry.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "string_hash.h"
+
+struct from_mash_in_place_constructor;
+struct mash_info_struct;
+
+struct entity_viseme_entry {
+    string_hash field_0;
+    int field_4;
+
+    entity_viseme_entry() = default;
+
+    entity_viseme_entry(from_mash_in_place_constructor *);
+
+    void unmash(mash_info_struct *, void *);
+};
