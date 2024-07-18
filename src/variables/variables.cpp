d33092170e0571588dcbcb7478d7fd86b05fcd40
MrMartinIden <MrMartinIden@protonmail.com>
2024-07-18 10:30:27 +0300

    Changed the type of global variables.

--- src/variables/variables.cpp
+++ src/variables/variables.cpp
@@ -7,117 +7,129 @@
 
 PolytubeCustomMaterial *& webline_texture = var<PolytubeCustomMaterial *>(0x00958058);
 
-Var<float> flt_87EBD4 = {0x87EBD4};
+float & g_strafe_mult = var<float>(0x00921B70);
 
-Var<float> flt_86F860 = {0x86F860};
+float & g_move_mult = var<float>(0x00921B6C);
 
-Var<int> dword_975308 = {0x00975308};
+float & sluggish_mix = var<float>(0x00959E44);
 
-Var<bool> byte_959561 = {0x00959561};
+float & med_mix = var<float>(0x00959E50);
 
-Var<int> dword_975314 = {0x00975314};
+float & fast_mix = var<float>(0x00959E38);
 
-Var<int> dword_97530C = {0x0097530C};
+float & pronto_mix = var<float>(0x00959E40);
 
-Var<bool> byte_971F9C = {0x00971F9C};
+float & slow_mix = var<float>(0x00959E4C);
 
-Var<bool> s_freeze_game_time = {0x009680AC};
+float & nglIFLSpeed = var<float>(0x0093BC78);
 
-Var<bool> g_generating_vtables = {0x0095A6F1};
+float & s_camera_target_radius_factor = var<float>(0x0087EBC4);
 
-Var<float> g_tan_half_fov_ratio{0x00921D7C};
+float & flt_87EBD4 = var<float>(0x0087EBD4);
 
-Var<bool> byte_965C20{0x00965C20};
+float & flt_86F860 = var<float>(0x0086F860);
 
-Var<bool> g_distance_clipping_enabled{0x0095C718};
+int & dword_975308 = var<int>(0x00975308);
 
-Var<int> g_distance_clipping{0x0095C2F8};
+bool & byte_959561 = var<bool>(0x00959561);
 
-Var<int> g_disable_occlusion_culling{0x0095C7AC};
+int & dword_975314 = var<int>(0x00975314);
 
-Var<int> globalTextLanguage{0x0096B430};
+int & dword_97530C = var<int>(0x0097530C);
+
+bool & byte_971F9C = var<bool>(0x00971F9C);
+
+bool & s_freeze_game_time = var<bool>(0x009680AC);
+
+bool & g_generating_vtables = var<bool>(0x0095A6F1);
+
+float & g_tan_half_fov_ratio = var<float>(0x00921D7C);
+
+LPDIRECTSOUNDBUFFER & dword_982570 = var<LPDIRECTSOUNDBUFFER>(0x00982570);
+
+void *& dword_982574 = var<void *>(0x00982574);
+
+bool & byte_965C20 = var<bool>(0x00965C20);
+
+bool & g_distance_clipping_enabled = var<bool>(0x0095C718);
+
+int & g_distance_clipping = var<int>(0x0095C2F8);
+
+int & g_disable_occlusion_culling = var<int>(0x0095C7AC);
+
+int & globalTextLanguage = var<int>(0x0096B430);
 
 HWND & g_appHwnd = var<HWND>(0x009874C4);
 
-Var<bool> g_is_the_packer{0x009682E4};
+bool & g_is_the_packer = var<bool>(0x009682E4);
 
-Var<bool> g_master_clock_is_up{0x00965EB0};
+bool & g_master_clock_is_up = var<bool>(0x00965EB0);
 
-Var<int> g_TOD{0x0091E000};
+int & g_TOD = var<int>(0x0091E000);
 
-Var<char *[14]> dword_965C24 { 0x00965C24 };
+char * (&dword_965C24)[14] = var<char *[14]>(0x00965C24);
 
-Var<char[1024]> g_scene_name = (0x0095C308);
+char (&g_scene_name)[1024] = var<char[1024]>(0x0095C308);
 
-Var<bool> bExit{0x0095C178};
+bool & bExit = var<bool>(0x0095C178);
 
-Var<bool> byte_965BF9 = {0x00965BF9};
+bool & byte_965BF9 = var<bool>(0x00965BF9);
 
+HCURSOR & hCursor = var<HCURSOR>(0x0096596C);
 
-Var<HCURSOR> hCursor = {0x0096596C};
+HANDLE & hEvent = var<HANDLE>(0x00965C5C);
 
-Var<HANDLE> hEvent{0x00965C5C};
+HANDLE & hObject = var<HANDLE>(0x00965C60);
 
-Var<HANDLE> hObject{0x00965C60};
+bool & byte_965BF5 = var<bool>(0x00965BF5);
+bool & byte_965BF6 = var<bool>(0x00965BF6);
 
-Var<bool> byte_965BF5 = {0x00965BF5};
-Var<bool> byte_965BF6 = {0x00965BF6};
+int & dword_922908 = var<int>(0x00922908);
 
-Var<int> dword_922908 = {0x00922908};
+bool & byte_922994 = var<bool>(0x00922994);
 
-Var<bool> byte_922994{0x00922994};
+IDirect3DDevice9 *& g_Direct3DDevice = var<IDirect3DDevice9 *>(0x00971F94);
 
-Var<IDirect3DDevice9 *> g_Direct3DDevice {0x00971F94};
+char & byte_965C21 = var<char>(0x00965C21);
 
-Var<char> byte_965C21 = {0x00965C21};
+char & byte_965BF8 = var<char>(0x00965BF8);
 
-Var<char> byte_965BF8 = {0x00965BF8};
+int & g_cx = var<int>(0x00965978);
+int & g_cy = var<int>(0x00965968);
 
-Var<int> g_cx = {0x00965978};
-Var<int> g_cy = {0x00965968};
+char *& dword_95C730 = var<char *>(0x0095C730);
+char *& dword_95C72C = var<char *>(0x0095C72C);
 
-Var<char *> dword_95C730 = {0x0095C730};
-Var<char *> dword_95C72C = {0x0095C72C};
+int & dword_91E1D8 = var<int>(0x0091E1D8);
 
-Var<int> dword_91E1D8{0x0091E1D8};
+bool & g_indoors = var<bool>(0x0095C87B);
 
-Var<bool> g_indoors{0x0095C87B};
+bool & g_player_shadows_enabled = var<bool>(0x00922C5C);
+bool & g_enable_stencil_shadows = var<bool>(0x00921C98);
 
-Var<bool> g_player_shadows_enabled = {0x00922C5C};
-Var<char> g_enable_stencil_shadows = {0x00921C98};
+int & g_cur_shadow_target = var<int>(0x00965F44);
 
-Var<int> g_cur_shadow_target = {0x00965F44};
+bool & byte_975468 = var<bool>(0x00975468);
 
-Var<bool> byte_975468{0x00975468};
+char (&byte_9659B8)[260] = var<char[260]>(0x009659B8);
 
-Var<string_hash> bip01_l_calf = {0x0095BA1C};
-Var<string_hash> bip01_r_calf = {0x0095AB1C};
-Var<string_hash> bip01_pelvis = {0x0095AAFC};
-Var<string_hash> bip01_head = {0x0095B9DC};
-Var<string_hash> bip01_spine = {0x0095BA18};
+int & nWidth = var<int>(0x0093AE84);
+int & nHeight = var<int>(0x0093AE88);
 
-Var<char[260]> byte_9659B8{0x009659B8};
+HWND & g_hWnd = var<HWND>(0x00971F98);
 
-Var<int> nWidth = {0x0093AE84};
-Var<int> nHeight = {0x0093AE88};
+float & flt_965BDC = var<float>(0x00965BDC);
 
-Var<HWND> g_hWnd = {0x00971F98};
+bool & ChromeEffect = var<bool>(0x0091E1D4);
 
-Var<float> flt_965BDC{0x00965BDC};
+int & g_Windowed = var<int>(0x00946530);
 
-Var<char> byte_95C718 = {0x0095C718};
-Var<int> dword_95C2F8 = {0x0095C2F8};
+float & flt_88E518 = var<float>(0x0088E518);
 
-Var<bool> ChromeEffect = {0x0091E1D4};
+float & flt_88E51C = var<float>(0x0088E51C);
 
-Var<int> g_Windowed = {0x00946530};
+IDirectSound8 *& g_directSound = var<IDirectSound8 *>(0x00987518);
 
-Var<float> flt_88E518{0x0088E518};
+bool & cam_target_locked = var<bool>(0x0095C754);
 
-Var<float> flt_88E51C{0x0088E51C};
-
-Var<IDirectSound8 *> pUnkOuter{0x00987518};
-
-Var<bool> cam_target_locked{0x0095C754};
-
-Var<bool> EnableShader{0x00972AB0};
+bool & EnableShader = var<bool>(0x00972AB0);
--- src/variables/variables.h
+++ src/variables/variables.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "variable.h"
-#include "../nlPlatformEnum.h"
-
+#include <dsound.h>
 #include <windows.h>
+
+#include <cstdint>
 
 struct string_hash;
 struct Input;
@@ -14,142 +14,135 @@ inline constexpr bool STANDALONE_SYSTEM = 1;
 
 extern PolytubeCustomMaterial *& webline_texture;
 
-inline Var<float> g_strafe_mult {0x00921B70};
+extern float & g_strafe_mult;
 
-inline Var<float> g_move_mult {0x00921B6C};
+extern float & g_move_mult;
 
-inline Var<float> sluggish_mix {0x00959E44};
+extern float & sluggish_mix;
 
-inline Var<float> med_mix {0x00959E50};
+extern float & med_mix;
 
-inline Var<float> fast_mix {0x00959E38};
+extern float & fast_mix;
 
-inline Var<float> pronto_mix {0x00959E40};
+extern float & pronto_mix;
 
-inline Var<float> slow_mix {0x00959E4C};
+extern float & slow_mix;
 
-inline Var<float> nglIFLSpeed {0x0093BC78};
+extern float & nglIFLSpeed;
 
-inline Var<float> s_camera_target_radius_factor {0x0087EBC4};
+extern float & s_camera_target_radius_factor;
 
-extern Var<float> flt_87EBD4;
+extern float & flt_87EBD4;
 
-extern Var<float> flt_86F860;
+extern float & flt_86F860;
 
-extern Var<bool> byte_959561;
+extern bool & byte_959561;
 
-extern Var<int> dword_975308;
+extern int & dword_975308;
 
-extern Var<int> dword_975314;
+extern int & dword_975314;
 
-extern Var<int> dword_97530C;
+extern int & dword_97530C;
 
-extern Var<bool> byte_971F9C;
+extern bool & byte_971F9C;
 
-extern Var<bool> s_freeze_game_time;
+extern bool & s_freeze_game_time;
 
-extern Var<bool> g_generating_vtables;
+extern bool & g_generating_vtables;
 
-extern Var<bool> byte_965C20;
+extern LPDIRECTSOUNDBUFFER & dword_982570;
 
-inline auto & g_platform = var<_nlPlatformEnum>(0x0095C1E4);
+extern void *& dword_982574;
 
-extern Var<bool> g_distance_clipping_enabled;
+extern bool & byte_965C20;
 
-extern Var<int> g_distance_clipping;
+extern bool & g_distance_clipping_enabled;
 
-extern Var<int> g_disable_occlusion_culling;
+extern int & g_distance_clipping;
 
-extern Var<int> globalTextLanguage;
+extern int & g_disable_occlusion_culling;
+
+extern int & globalTextLanguage;
 
 extern HWND & g_appHwnd;
 
-extern Var<bool> g_is_the_packer;
+extern bool & g_is_the_packer;
 
-extern Var<int> g_TOD;
+extern int & g_TOD;
 
-extern Var<char *[14]> dword_965C24;
+extern char * (&dword_965C24)[14];
 
-extern Var<char[1024]> g_scene_name;
+extern char (&g_scene_name)[1024];
 
-extern Var<bool> bExit;
+extern bool & bExit;
 
-extern Var<bool> byte_965BF9;
+extern bool & byte_965BF9;
 
-extern Var<HCURSOR> hCursor;
+extern HCURSOR & hCursor;
 
-extern Var<HANDLE> hEvent;
+extern HANDLE & hEvent;
 
-extern Var<HANDLE> hObject;
+extern HANDLE & hObject;
 
-extern Var<bool> byte_965BF5;
-extern Var<bool> byte_965BF6;
+extern bool & byte_965BF5;
+extern bool & byte_965BF6;
 
 struct IDirect3DDevice9;
-extern Var<IDirect3DDevice9 *> g_Direct3DDevice;
+extern IDirect3DDevice9 *& g_Direct3DDevice;
 
-extern Var<float> g_tan_half_fov_ratio;
+extern float & g_tan_half_fov_ratio;
 
-extern Var<int> dword_922908;
+extern int & dword_922908;
 
-extern Var<bool> byte_922994;
+extern bool & byte_922994;
 
-extern Var<char> byte_965C21;
+extern char & byte_965C21;
 
-extern Var<char> byte_965BF8;
+extern char & byte_965BF8;
 
-extern Var<int> g_cx;
-extern Var<int> g_cy;
+extern int & g_cx;
+extern int & g_cy;
 
-extern Var<HWND> g_hWnd;
+extern HWND & g_hWnd;
 
-extern Var<char *> dword_95C730;
-extern Var<char *> dword_95C72C;
+extern char *& dword_95C730;
+extern char *& dword_95C72C;
 
-extern Var<int> dword_91E1D8;
+extern int & dword_91E1D8;
 
-extern Var<bool> g_indoors;
+extern bool & g_indoors;
 
-extern Var<bool> g_player_shadows_enabled;
-extern Var<char> g_enable_stencil_shadows;
+extern bool & g_player_shadows_enabled;
+extern bool & g_enable_stencil_shadows;
 
-extern Var<int> g_cur_shadow_target;
+extern int & g_cur_shadow_target;
 
-extern Var<string_hash> bip01_l_calf;
-extern Var<string_hash> bip01_r_calf;
-extern Var<string_hash> bip01_pelvis;
-extern Var<string_hash> bip01_head;
-extern Var<string_hash> bip01_spine;
+extern char (&byte_9659B8)[260];
 
-extern Var<char[260]> byte_9659B8;
+extern int & nWidth;
+extern int & nHeight;
 
-extern Var<int> nWidth;
-extern Var<int> nHeight;
+extern float & flt_965BDC;
 
-extern Var<float> flt_965BDC;
+extern bool & ChromeEffect;
 
-extern Var<char> byte_95C718;
-extern Var<int> dword_95C2F8;
-
-extern Var<bool> ChromeEffect;
-
-extern Var<int> g_Windowed;
+extern int & g_Windowed;
 
 struct IDirectSound8;
 
-extern Var<float> flt_88E518;
+extern float & flt_88E518;
 
-extern Var<float> flt_88E51C;
+extern float & flt_88E51C;
 
-extern Var<IDirectSound8 *> pUnkOuter;
+extern IDirectSound8 *& g_directSound;
 
-extern Var<bool> g_master_clock_is_up;
+extern bool & g_master_clock_is_up;
 
-extern Var<bool> byte_975468;
+extern bool & byte_975468;
 
-extern Var<bool> cam_target_locked;
+extern bool & cam_target_locked;
 
-extern Var<bool> EnableShader;
+extern bool & EnableShader;
 
 inline constexpr uint32_t RESOURCE_VERSION_INVALID = 0xFFFFFFFF;
 
