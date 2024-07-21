cee929448ed10f5c5c32a0ee3e2b5605634c3ac3
MrMartinIden <MrMartinIden@protonmail.com>
2024-07-21 09:17:49 +0300

    Added functions for event_type.

--- src/event_type.cpp
+++ src/event_type.cpp
@@ -2,6 +2,7 @@
 
 #include "binary_search_array_cmp.h"
 #include "common.h"
+#include "event.h"
 #include "event_callback.h"
 #include "event_recipient_entry.h"
 #include "event_type.h"
@@ -9,8 +10,77 @@
 
 VALIDATE_SIZE(event_type, 0x2C);
 
-event_type::event_type(string_hash a2, bool a3) {
-    THISCALL(0x004E18B0, this, a2, a3);
+event_type::event_type(string_hash a2, bool a3)
+{
+    if constexpr (0) {
+    } else {
+        THISCALL(0x004E18B0, this, a2, a3);
+    }
+}
+
+event_type::~event_type()
+{
+    TRACE("event_type::~event_type");
+
+    this->clear();
+
+    if ( this->event_to_raise != nullptr ) {
+        this->event_to_raise->_finalize(true);
+    }
+
+    this->field_1C.clear();
+    this->field_8.clear();
+}
+
+void * event_type::operator new(size_t sz) {
+    return mem_alloc(sz);
+}
+
+void event_type::operator delete(void *ptr, size_t sz) {
+    mem_dealloc(ptr, sz);
+}
+
+void event_type::clear()
+{
+    TRACE("event_type::clear");
+
+    this->field_0 = {0};
+
+    for ( auto &entry : this->field_8 )
+    {
+        if ( entry != nullptr ) {
+            delete entry;
+        }
+    }
+
+    this->field_8.clear();
+    this->field_18 = false;
+    this->clear_callbacks();
+}
+
+void event_type::clear_callbacks()
+{
+    TRACE("event_type::clear_callbacks");
+
+    if constexpr (1)
+    {
+        for ( auto &entry : this->field_8 ) {
+            entry->clear_callbacks();
+        }
+
+        for ( auto &cb : this->field_1C )
+        {
+            if ( cb != nullptr ) {
+                cb->_finalize(true);
+            }
+        }
+
+        this->field_1C.clear();
+    }
+    else
+    {
+        THISCALL(0x004D1ED0, this);
+    }
 }
 
 event_recipient_entry * event_type::find_recipient_entry(entity_base_vhandle a2)
@@ -105,7 +175,6 @@ void event_type::remove_default_callback(unsigned int a2)
 
 void event_type::clear_script_callbacks(entity_base_vhandle a2, script_executable *a3)
 {
-
     for ( auto &v1 : this->field_8 )
     {
         if ( v1->field_0 == a2 ) {
@@ -116,6 +185,8 @@ void event_type::clear_script_callbacks(entity_base_vhandle a2, script_executabl
 
 bool event_type::garbage_collect()
 {
+    TRACE("event_type::garbage_collect");
+
     bool (__fastcall *func)(void *) = CAST(func, 0x004D65B0);
     return func(this);
 }
--- src/event_type.h
+++ src/event_type.h
@@ -19,6 +19,19 @@ struct event_type {
     //0x004E18B0
     event_type(string_hash a2, bool a3);
 
+    //0x004E1970
+    ~event_type();
+
+    void * operator new(size_t sz);
+
+    void operator delete(void *ptr, size_t sz);
+
+    //0x004DB890
+    void clear();
+
+    //0x004D1ED0
+    void clear_callbacks();
+
     event_recipient_entry * find_recipient_entry(entity_base_vhandle a2);
 
     //0x004EE6C0
