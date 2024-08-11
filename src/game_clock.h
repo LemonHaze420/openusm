943bda32d1f1a116c266be32078cd0276cc4abb0
MrMartinIden <MrMartinIden@protonmail.com>
2024-08-11 15:00:57 +0300

    Added static variable game_clock::ticks

--- src/game_clock.h
+++ src/game_clock.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "float.hpp"
+#include "variable.h"
 
 struct game_clock {
     //0x0058E2F0
     static void frame_advance(Float a1);
+
+    static inline auto & ticks = var<Float>(0x00965AC4);
 };
