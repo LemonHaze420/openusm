7cf0ef36b5c1ffd4398b8277957e30e3bf75f7ba
MrMartinIden <MrMartinIden@protonmail.com>
2024-08-28 23:26:36 +0300

    Added size function for mContainer_base.

--- src/mcontainer_base.h
+++ src/mcontainer_base.h
@@ -18,6 +18,10 @@ struct mContainer_base {
 
     void clear();
 
+    auto size() const {
+        return this->m_size;
+    }
+
     bool is_pointer_in_mash_image(void *a3) const;
 
     void destruct_mashed_class();
