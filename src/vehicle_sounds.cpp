bfbaef7c480775466e1cc070f67e40d4ca7fb73e
MrMartinIden <MrMartinIden@protonmail.com>
2025-02-02 21:14:42 +0300

    Added vehicle_sounds::manage_engine_sounds function.

--- src/vehicle_sounds.cpp
+++ src/vehicle_sounds.cpp
@@ -1,10 +1,19 @@
 #include "vehicle_sounds.h"
 
 #include "common.h"
+#include "func_wrapper.h"
 
 VALIDATE_SIZE(vehicle_sounds, 0x50);
 
 vehicle_sounds::vehicle_sounds()
 {
 
+}
+
+void vehicle_sounds::manage_engine_sounds(Float a2, bool a3)
+{
+    if constexpr (0) {
+    } else {
+        THISCALL(0x006BB690, this, a2, a3);
+    }
 }
