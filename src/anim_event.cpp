3c345136beb8609b556cf9ff9751d145f582e22e
MrMartinIden <MrMartinIden@protonmail.com>
2025-02-17 00:38:44 +0300

    Added constructor and destructor for anim_event

--- src/anim_event.cpp
+++ src/anim_event.cpp
@@ -1,5 +1,16 @@
 #include "anim_event.h"
 
 #include "common.h"
+#include "func_wrapper.h"
 
 VALIDATE_SIZE(anim_event, 0x18);
+
+anim_event::anim_event(string_hash a2, string_hash a3, int count) : event(a2)
+{
+    THISCALL(0x004AD3F0, this, a2, a3, count);
+}
+
+anim_event::~anim_event()
+{
+    THISCALL(0x0043A570, this);
+}
--- src/anim_event.h
+++ src/anim_event.h
@@ -2,6 +2,12 @@
 
 struct anim_event : event {
     string_hash field_C;
-    void *field_10;
+    string_hash *field_10;
     int my_num_params;
+
+    //0x004AD3F0
+    anim_event(string_hash a2, string_hash a3, int count);
+
+    //0x0043A570
+    ~anim_event();
 };
