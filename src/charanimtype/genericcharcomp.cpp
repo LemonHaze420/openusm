592b8bd0b3b7f47833d911e177779ef90f347932
MrMartinIden <MrMartinIden@protonmail.com>
2025-06-17 08:59:12 +0300

    Added GenericCharComp::CalcPoseDataDirect functions.

--- src/charanimtype/genericcharcomp.cpp
+++ src/charanimtype/genericcharcomp.cpp
@@ -3,6 +3,8 @@
 #include "charcomponentmanager.h"
 #include "common.h"
 #include "nal_generic.h"
+#include "trace.h"
+#include "utility.h"
 
 GenericCharComp::GenericCharComp()
 {
@@ -19,3 +21,33 @@ void GenericCharComp::BuildBoneMatrices(
 {
     bit_cast<const nalGeneric::nalGenericSkeleton *>(a3)->GetBoneMatrices(bit_cast<const nalGeneric::nalGenericPose *>(a4), a1);
 }
+
+void GenericCharComp::CalcPoseDataDirect(
+        void *a3,
+        uint32_t ,
+        Float arg8,
+        Float a5,
+        const nalComp::nalCompAnim *,
+        const void *,
+        const void *,
+        const void *,
+        void *a10)
+{
+    TRACE("GenericCharComp::CalcPoseDataDirect");
+
+    nalGeneric::nalGenericPose a1 {*static_cast<nalGeneric::nalGenericPose *>(a3), true};
+    static_cast<nalGeneric::nalGenericInstance *>(a10)->GetPose(
+            arg8,
+            a5,
+            *static_cast<nalGeneric::nalGenericPose *>(a3),
+            a1);
+}
+
+void GenericCharComp_patch()
+{
+    {
+        FUNC_ADDRESS(address, &GenericCharComp::CalcPoseDataDirect);
+        set_vfunc(0x00892170, address);
+    }
+}
+
--- src/charanimtype/genericcharcomp.h
+++ src/charanimtype/genericcharcomp.h
@@ -2,6 +2,9 @@
 
 #include "charcomponentbase.h"
 
+#include <float.hpp>
+
+
 struct GenericCharComp : CharComponentBase {
     GenericCharComp();
 
@@ -11,4 +14,18 @@ struct GenericCharComp : CharComponentBase {
         uint32_t a2,
         const void *a3,
         const void *a4);
+
+    //virtual
+    void CalcPoseDataDirect(
+        void *a3,
+        uint32_t _24,
+        Float arg8,
+        Float a5,
+        const nalComp::nalCompAnim *a6,
+        const void *a7,
+        const void *a8,
+        const void *a9,
+        void *a10);
 };
+
+extern void GenericCharComp_patch();
