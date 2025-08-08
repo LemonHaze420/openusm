f94c7732a2575711384582c240d70761e3aa7e3a
LemonHaze420 <35783139+LemonHaze420@users.noreply.github.com>
2025-08-08 17:15:25 +0100

    disabled scripting hooks

--- tools/ModLoader/dllmain.cpp
+++ tools/ModLoader/dllmain.cpp
@@ -26,10 +26,10 @@ void init_hooks()
                 {(void*)0x0077A870, reinterpret_cast<void*>(hk_nglLoadTextureTM2), reinterpret_cast<void**>(&nglLoadTextureTM2)},
                 {(void*)0x00531B30, reinterpret_cast<void*>(hk_get_resource), reinterpret_cast<void**>(&get_resource_orig)},
                 {(void*)0x0052AA70, reinterpret_cast<void*>(hk_get_resource_dir), reinterpret_cast<void**>(&get_resource_dir_orig)},
-                {(void*)0x0058EDE0, reinterpret_cast<void*>(hk_script_func_reg), reinterpret_cast<void**>(&script_func_reg_orig)},
-                {(void*)0x0058EE30, reinterpret_cast<void*>(hk_script_func), reinterpret_cast<void**>(&script_func_orig)},
-                {(void*)0x0064E740, reinterpret_cast<void*>(hk_exec), reinterpret_cast<void**>(&exec_orig)},
-                {(void*)0x005AF9F0, reinterpret_cast<void*>(hk_script_manager_run), reinterpret_cast<void**>(&script_manager_run_orig)},
+                //{(void*)0x0058EDE0, reinterpret_cast<void*>(hk_script_func_reg), reinterpret_cast<void**>(&script_func_reg_orig)},
+                //{(void*)0x0058EE30, reinterpret_cast<void*>(hk_script_func), reinterpret_cast<void**>(&script_func_orig)},
+                //{(void*)0x0064E740, reinterpret_cast<void*>(hk_exec), reinterpret_cast<void**>(&exec_orig)},
+                //{(void*)0x005AF9F0, reinterpret_cast<void*>(hk_script_manager_run), reinterpret_cast<void**>(&script_manager_run_orig)},
             };
 
             for (auto& hook : hooks) {
