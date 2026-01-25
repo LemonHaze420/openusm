6f0a91497846c02a5cd7ca6792211eb9d7d31ba3
MrMartinIden <MrMartinIden@protonmail.com>
2026-01-25 20:45:46 +0300

    Added new functions for base_ai_data struct.

--- src/base_ai_data.cpp
+++ src/base_ai_data.cpp
@@ -7,6 +7,12 @@
 VALIDATE_SIZE(base_ai_data, 0x18);
 VALIDATE_OFFSET(base_ai_data, field_14, 0x14);
 
+base_ai_data::base_ai_data(from_mash_in_place_constructor *a2)
+{
+    void (__fastcall *func)(void *, void *edx, from_mash_in_place_constructor *) = CAST(func, 0x006D9AF0);
+    func(this, nullptr, a2);
+}
+
 void base_ai_data::post_entity_mash() {
     this->field_14->post_entity_mash();
 }
@@ -22,4 +28,10 @@ void base_ai_data::destruct_mashed_class()
 
     this->field_0.destruct_mashed_class();
     this->field_8.destruct_mashed_class();
+}
+
+void base_ai_data::unmash(mash_info_struct *a1, void *)
+{
+    a1->unmash_class_in_place(this->field_0, this);
+    a1->unmash_class_in_place(this->field_8, this);
 }
--- src/base_ai_data.h
+++ src/base_ai_data.h
@@ -7,14 +7,22 @@ namespace ai {
 struct ai_core;
 }
 
+struct from_mash_in_place_constructor;
+
 struct base_ai_data {
     resource_key field_0;
     ai::param_block field_8;
     ai::ai_core *field_14;
+
+    //0x006D9AF0
+    base_ai_data(from_mash_in_place_constructor *a2);
 
     //0x006BDB00
     void post_entity_mash();
 
     //0x006D7310
     void destruct_mashed_class();
+
+    //0x006D7370
+    void unmash(mash_info_struct *a1, void *a3);
 };
