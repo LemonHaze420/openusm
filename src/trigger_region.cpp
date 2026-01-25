fdc0b9a09af9d866f42195fc6715c2e62cf14c12
MrMartinIden <MrMartinIden@protonmail.com>
2026-01-25 20:53:50 +0300

    Added new functions for trigger_region struct.

--- src/trigger_region.cpp
+++ src/trigger_region.cpp
@@ -1,1 +1,17 @@
 #include "trigger_region.h"
+
+#include "common.h"
+
+VALIDATE_SIZE(trigger_region, 0x4);
+
+
+void trigger_region::unmash(mash_info_struct *, void *)
+{
+	;
+}
+
+
+int trigger_region::get_mash_sizeof() const
+{
+	return 4;
+}
--- src/trigger_region.h
+++ src/trigger_region.h
@@ -2,4 +2,11 @@
 
 #include "mash_virtual_base.h"
 
-struct trigger_region : mash_virtual_base {};
+struct trigger_region : mash_virtual_base {
+
+	//virtual
+	void unmash(mash_info_struct *, void *);
+
+	//virtual
+	int get_mash_sizeof() const;
+};
