8bc58e0030e6ae4ec94454f0c3c4a91aae928e27
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-05 00:15:25 +0300

    Added unit test of string_hash for standalone option.

--- test/string_hash.cpp
+++ test/string_hash.cpp
@@ -1,9 +1,26 @@
 #include <gtest/gtest.h>
 
-#include <string_hash.h>
+#include <os_developer_options.h>
+#include <string_hash_dictionary.h>
 
 TEST(StringHash, Construct)
 {
     string_hash hash {};
     EXPECT_EQ(0, hash.source_hash_code);
 }
+
+#ifdef SETUP_STANDALONE_SYSTEM
+
+TEST(StringHash, ConstructWithString)
+{
+    os_developer_options::os_developer_init();
+
+    g_platform = NL_PLATFORM_PC;
+
+    string_hash_dictionary::create_inst();
+
+    string_hash hash {"ultimate_spiderman"};
+    EXPECT_TRUE(hash.source_hash_code != 0);
+}
+
+#endif
