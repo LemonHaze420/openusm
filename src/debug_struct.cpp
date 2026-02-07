366a3348004466a797b2010212aa03a57d978510
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-07 20:03:55 +0300

    Refactored debug_struct.

--- src/debug_struct.cpp
+++ src/debug_struct.cpp
@@ -1,11 +1,14 @@
 #include "debug_struct.h"
 
 #include "variable.h"
+#include "variables.h"
 
 #if !STANDALONE_SYSTEM
 debug_struct_t & g_debug = var<debug_struct_t>(0x0095FF80);
 #else
-static debug_struct_t g_debug1 {};
-debug_struct_t & g_debug = g_debug1;
+debug_struct_t & g_debug = []() -> auto & {
+    static debug_struct_t g_debug1 {};
+    return g_debug1;
+}();
 #endif
 
