32bd0c6be9e344259ed4a39816476dd189ba7eb4
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-11 12:23:41 +0300

    Added nalComponentRLE8Int1 struct.

---
+++ src/nal/include/common/nal_generic_component.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "nal_component.h"
+
+#include "nal_system.h"
+
+struct nalComponentRLE8Int1Data {
+};
+
+struct nalComponentRLE8Int1 : nalComponent<nalComponentU8Base, nalComponentRLE8Int1Data, nalComponentRLE8Int1> {
+
+    nalComponentRLE8Int1();
+};
+
+extern nalComponentRLE8Int1 & Component_nalComponentRLE8Int1;
---
+++ src/nal/source/common/nal_generic_component.cpp
@@ -0,0 +1,28 @@
+#include "nal_generic_component.h"
+
+#include "common.h"
+#include "variables.h"
+
+#if !STANDALONE_SYSTEM
+nalComponentRLE8Int1 & Component_nalComponentRLE8Int1 = var<nalComponentRLE8Int1>(0x00946A90);
+#else
+nalComponentRLE8Int1 & Component_nalComponentRLE8Int1 = []() -> auto & {
+    static nalComponentRLE8Int1 g_Component_nalComponentRLE8Int1 {};
+    return g_Component_nalComponentRLE8Int1;
+}();
+#endif
+
+nalComponentRLE8Int1::nalComponentRLE8Int1() {
+    if constexpr (1) {
+        static void * g_vtbl[] {
+            func_address(&_GetType),
+            nullptr,
+            nullptr,
+            nullptr,
+            func_address(&_Process)
+        };
+        this->m_vtbl = CAST(m_vtbl, &g_vtbl);
+    } else {
+        this->m_vtbl = 0x008BD4B8;
+    }
+}
