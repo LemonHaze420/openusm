2e2c4a7ffc881ea1ec5bc301705d1a81d41e23c7
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-11 12:28:30 +0300

    Added PanelComponent struct.

---
+++ src/panels/nalpanel/panel_component.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "component.h"
+
+struct PanelComponent : BaseComponent {
+    PanelComponent *m_prevComp;
+
+    PanelComponent();
+};
+
+namespace PanelComponentMgr {
+    extern PanelComponent *& comp_list;
+
+    extern void Add(PanelComponent *);
+}
