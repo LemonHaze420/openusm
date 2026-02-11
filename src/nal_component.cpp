f59f3b71c033962852434e607bc2c95beafacb5c
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-11 12:21:59 +0300

    Refactored nalComponent.

--- src/nal_component.cpp
+++ src/nal_component.cpp
@@ -4,10 +4,6 @@
 #include "spidey_signal.h"
 
 template<>
-nalComponent<nalComponentU8Base, spideySignalData, spideySignal>::nalComponent() {
-}
-
-template<>
 void nalComponent<nalComponentU8Base, spideySignalData, spideySignal>::_Process(
     const nalGeneric::nalComponentInfo *a1, void *&a2, void *&) {
     for (int i = 0; i < a1->field_28; ++i) {
--- src/nal_component.h
+++ src/nal_component.h
@@ -2,16 +2,28 @@
 
 #include <cstdint>
 
+#include <nal_generic.h>
+#include "utility.h"
 #include "vtbl.h"
-
-namespace nalGeneric {
-struct nalComponentInfo;
-}
 
 template<class T0, class T1, class T2>
 struct nalComponent : T0, T1 {
 
-    nalComponent();
+    nalComponent() {
+        static void * g_vtbl[] {
+            func_address(&T0::_GetType),
+            nullptr,
+            nullptr,
+            nullptr,
+            func_address(&_Process)
+        };
 
-    void _Process(const nalGeneric::nalComponentInfo *a1, void *&a2, void *&);
+        T0::m_vtbl = CAST(T0::m_vtbl, &g_vtbl);
+    }
+
+    void _Process(const nalGeneric::nalComponentInfo *a1, void *&a2, void *&) {
+		for (int i = 0; i < a1->field_28; ++i) {
+			a2 = static_cast<char *>(a2) + 1;
+		}
+	}
 };
