8fbe6246e8a8b8e25f9acbf4281843aff0ef6b94
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-15 09:38:21 +0300

    Refactored scene_anim_resource_handler struct.

--- src/scene_anim_resource_handler.cpp
+++ src/scene_anim_resource_handler.cpp
@@ -14,9 +14,29 @@ VALIDATE_SIZE(scene_anim_resource_handler, 0x14);
 
 scene_anim_resource_handler::scene_anim_resource_handler(worldly_pack_slot *a2)
 {
-    this->m_vtbl = 0x008889E8;
+    if constexpr (1) {
+        static void * g_vtbl[] = {
+            func_address(&finalize),
+            func_address(&_handle),
+            func_address(&_pre_handle_resources),
+            func_address(&_handle_resource),
+        };
+
+        this->m_vtbl = CAST(m_vtbl, &g_vtbl);
+    } else {
+        this->m_vtbl = 0x008889E8;
+    }
+
     this->my_slot = a2;
     this->field_10 = TLRESOURCE_TYPE_SCENE_ANIM;
+}
+
+void scene_anim_resource_handler::finalize(bool a2)
+{
+    this->~scene_anim_resource_handler();
+    if (a2) {
+        delete(this);
+    }
 }
 
 bool scene_anim_resource_handler::_handle(worldly_resource_handler::eBehavior a2, limited_timer *a3)
--- src/scene_anim_resource_handler.h
+++ src/scene_anim_resource_handler.h
@@ -5,6 +5,9 @@
 struct scene_anim_resource_handler : base_tl_resource_handler {
     scene_anim_resource_handler(worldly_pack_slot *a2);
 
+    //virtual
+    void finalize(bool a2);
+
     bool _handle(worldly_resource_handler::eBehavior a2, limited_timer *a3);
 
     //0x0055F990
