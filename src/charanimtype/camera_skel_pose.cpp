5ad57e5b6db9475ec3c6305df6f9d91ead39e0a8
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-15 10:14:54 +0300

    Added new functions for nalCamSkeleton struct and refactored.

--- src/charanimtype/camera_skel_pose.cpp
+++ src/charanimtype/camera_skel_pose.cpp
@@ -2,7 +2,49 @@
 
 #include "common.h"
 #include "variable.h"
+#include "variables.h"
 
 namespace nalCam {
+
+    VALIDATE_SIZE(nalCamPose, 0x30u);
+
+#if !STANDALONE_SYSTEM
     int & nalCamSkeleton::vtbl_ptr = var<int>(0x0096A780);
+#else
+    int & nalCamSkeleton::vtbl_ptr = []() -> auto & {
+        static nalCamSkeleton cam {};
+        return cam.m_vtbl;
+    }();
+#endif
+}
+
+nalCam::nalCamSkeleton::nalCamSkeleton()
+{
+    if constexpr (1) {
+        static void * g_vtbl[] {
+            nullptr,
+            nullptr,
+            func_address(&_Process),
+            nullptr,
+            func_address(&_CheckVersion)
+        };
+        this->m_vtbl = CAST(m_vtbl, &g_vtbl);
+    } else {
+        this->m_vtbl = 0x00892064;
+    }
+}
+
+void nalCam::nalCamSkeleton::_Process()
+{
+    this->field_60.field_0 = this;
+}
+
+void nalCam::nalCamSkeleton::_Release()
+{
+    this->field_60.field_0 = nullptr;
+}
+
+bool nalCam::nalCamSkeleton::_CheckVersion()
+{
+    return this->Version == 0x10000;
 }
--- src/charanimtype/camera_skel_pose.h
+++ src/charanimtype/camera_skel_pose.h
@@ -1,17 +1,30 @@
 #pragma once
 
+#include "nal_skeleton.h"
+
 namespace nalCam {
 
-    struct nalCamSkeleton {
-        struct vtbl {
-            void *field_0;
-            void *finalize;
-            void *Process;
-            void *Release;
+    struct nalCamSkeleton;
 
-            using CheckVersion_t = bool (nalCamSkeleton::*)();
-            CheckVersion_t CheckVersion;
-        };
+    struct nalCamPose {
+        nalCamSkeleton *field_0;
+        char field_4[0x2C];
+    };
+
+    struct nalCamSkeleton : nalBaseSkeleton {
+        int field_5C;
+        nalCamPose field_60;
+
+        nalCamSkeleton();
+
+        //virtual
+        void _Process();
+
+        //virtual
+        void _Release();
+
+        //virtual
+        bool _CheckVersion();
 
         static int & vtbl_ptr;
     };
