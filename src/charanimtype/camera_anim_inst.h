4ff2fc62d04b59db032fe96fed536d612e1d3d7e
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-15 10:13:55 +0300

    Refactored nalCamAnim struct.

--- src/charanimtype/camera_anim_inst.h
+++ src/charanimtype/camera_anim_inst.h
@@ -1,18 +1,34 @@
 #pragma once
 
+#include "nal_anim.h"
+#include "nal_system.h"
+
 namespace nalCam {
 
-    struct nalCamAnim {
-        struct vtbl {
-            void *field_0;
-            void *finalize;
-            void *Process;
-            void *Release;
+    struct nalCamInstance;
+    struct nalCamSkeleton;
 
-            using CheckVersion_t = bool (nalCamAnim::*)();
-            CheckVersion_t CheckVersion;
-        };
+    struct nalCamAnim : nalAnimClass<nalAnyPose> {
+
+        nalCamAnim();
+
+        //0x005FCD80
+        nalCamInstance * CreateInstance(nalCamSkeleton *a2);
+
+        //virtual
+        //0x005FCD70
+        nalCamInstance * _VirtualCreateInstance(nalBaseSkeleton *a1);
 
         static int & vtbl_ptr;
+    };
+
+    struct nalCamBaseInstance : nalAnimClass<nalAnyPose>::nalInstanceClass {
+        nalCamBaseInstance(nalAnimClass<nalAnyPose> *, nalBaseSkeleton *);
+    };
+
+    struct nalCamInstance : nalCamBaseInstance {
+
+        nalCamInstance(nalCamAnim *a2, nalCamSkeleton *a3);
+
     };
 }
