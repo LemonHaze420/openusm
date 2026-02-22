06269d3c4f27fa13a8bedafda49fed81592fe357
MrMartinIden <MrMartinIden@protonmail.com>
2026-02-22 16:25:13 +0300

    Refactored material_file_resource_handler struct.

--- src/material_file_resource_handler.cpp
+++ src/material_file_resource_handler.cpp
@@ -13,7 +13,19 @@ VALIDATE_SIZE(material_file_resource_handler, 0x14);
 
 material_file_resource_handler::material_file_resource_handler(worldly_pack_slot *a2)
 {
-    this->m_vtbl = 0x00888A94;
+    if constexpr (1) {
+        static void * g_vtbl[] = {
+            func_address(&finalize),
+            func_address(&_handle),
+            func_address(&_pre_handle_resources),
+            func_address(&_handle_resource),
+        };
+
+        this->m_vtbl = CAST(m_vtbl, &g_vtbl);
+    } else {
+        this->m_vtbl = 0x00888A94;
+    }
+
     this->my_slot = a2;
     this->field_10 = TLRESOURCE_TYPE_MATERIAL_FILE;
 }
