66e4d308b43e15d4f8c22eda5b6980c4fcfc9484
MrMartinIden <MrMartinIden@protonmail.com>
2026-03-18 20:57:45 +0300

    Implemented Init method and constructor, added fields for fe_mission_text.

--- src/fe_mission_text.cpp
+++ src/fe_mission_text.cpp
@@ -1,11 +1,91 @@
 #include "fe_mission_text.h"
 
+#include "common.h"
+#include "fetext.h"
 #include "func_wrapper.h"
+#include "panelfile.h"
+#include "panelquad.h"
 #include "utility.h"
+#include "variables.h"
+
+#include <d3dx9tex.h>
+
+VALIDATE_SIZE(fe_mission_text, 0xB8u);
 
 fe_mission_text::fe_mission_text()
 {
+#if STANDALONE_SYSTEM
+    if constexpr (1)
+#else
+    if constexpr (0)
+#endif
+    {
+        this->m_vtbl = CAST(m_vtbl, &g_vtbl);
+    } else {
+        this->m_vtbl = 0x00893E70;
+    }
 
+    this->field_4 = nullptr;
+    this->field_B4 = false;
+    this->field_18 = nullptr;
+}
+
+void fe_mission_text::Init()
+{
+    assert(this->field_4 == nullptr && "Mission text widget already loaded.");
+
+    this->field_4 = PanelFile::UnmashPanelFile("text_box_small", static_cast<panel_layer>(7));
+
+    this->field_AC = this->field_4->field_28.at(0);
+    this->field_8 = this->field_4->GetPQ("tb_small_back_01");
+    this->field_C = this->field_4->GetPQ("tb_small_back_02");
+    this->field_10 = this->field_4->GetPQ("tb_detail_01");
+    this->field_9C = this->field_4->GetPQ("tb_small_gradient_text");
+    this->field_A0 = this->field_4->GetPQ("tb_small_gradient_hints");
+    this->field_A4 = this->field_4->GetPQ("tb_small_gradient_reward");
+    this->field_A8 = this->field_4->GetTextPointer("tb_body_text_EMPTY");
+
+    this->field_8->TurnOn(true);
+    this->field_C->TurnOn(true);
+    this->field_10->TurnOn(true);
+    this->field_9C->TurnOn(true);
+    this->field_A0->TurnOn(true);
+    this->field_A4->TurnOn(true);
+    this->field_A8->TurnOn(true);
+
+    {
+        FEText::string v22 {{}};
+        this->field_A8->SetTextNoLocalize(v22);
+    }
+
+    this->field_A8->SetNoFlash(color32 {0xC8, 0xC8, 0xC8, 0xFF});
+
+    this->field_14 = new PanelQuad {};
+    this->field_14->CopyFrom(this->field_A0);
+
+    D3DXCreateTextureFromFileExW(
+            g_Direct3DDevice,
+            L"data\\packs\\back_panel.dat",
+            0,
+            0,
+            1,
+            0,
+            D3DFMT_A8R8G8B8,
+            D3DPOOL_MANAGED,
+            3,
+            3,
+            0,
+            nullptr,
+            nullptr,
+            (IDirect3DTexture9 **)&this->field_18);
+
+    this->field_1C.DXTexture = (IDirect3DTexture9 *)this->field_18;
+    this->field_14->SetTexture(&this->field_1C);
+    this->field_A8->SetNumLines(4);
+
+    this->field_B0 = 0;
+    this->field_B5 = false;
+    this->field_B6 = false;
 }
 
 void fe_mission_text::set_flavor(int a2) {
@@ -24,7 +104,13 @@ void fe_mission_text::SetShown(bool a2) {
     THISCALL(0x0061AA00, this, a2);
 }
 
-void fe_mission_text_patch() {
+void fe_mission_text_patch()
+{
+    {
+        FUNC_ADDRESS(address, &fe_mission_text::Init);
+        REDIRECT(0x00647E1A, address);
+    }
+
     {
         FUNC_ADDRESS(address, &fe_mission_text::set_text);
         REDIRECT(0x00672EBC, address);
--- src/fe_mission_text.h
+++ src/fe_mission_text.h
@@ -1,12 +1,38 @@
 #pragma once
 
 #include "mstring.h"
+#include "ngl.h"
+
+struct FEText;
+struct PanelAnimFile;
+struct PanelFile;
+struct PanelQuad;
+struct IDirect3DBaseTexture9;
 
 struct fe_mission_text {
-    int field_0[44];
+    std::intptr_t m_vtbl;
+    PanelFile *field_4;
+    PanelQuad *field_8;
+    PanelQuad *field_C;
+    PanelQuad *field_10;
+    PanelQuad *field_14;
+    IDirect3DBaseTexture9 *field_18;
+    nglTexture field_1C;
+    PanelQuad *field_9C;
+    PanelQuad *field_A0;
+    PanelQuad *field_A4;
+    FEText *field_A8;
+    PanelAnimFile *field_AC;
     int field_B0;
+    bool field_B4;
+    bool field_B5;
+    bool field_B6;
 
+    //0x0060D0F0
     fe_mission_text();
+
+    //0x00643A80
+    void Init();
 
     //0x0060D560
     void set_flavor(int a2);
@@ -22,6 +48,10 @@ struct fe_mission_text {
 
     //0x0061AA00
     void SetShown(bool a2);
+
+    static inline void * g_vtbl[] {
+        nullptr
+    };
 };
 
 extern void fe_mission_text_patch();
