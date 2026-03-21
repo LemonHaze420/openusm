15f15383aaf5a0bc29048ff4204897d2881a405a
MrMartinIden <MrMartinIden@protonmail.com>
2026-03-21 22:21:32 +0300

    Implemented init function and constructor, added fields for targeting_reticle struct.

--- src/targeting_reticle.cpp
+++ src/targeting_reticle.cpp
@@ -1,5 +1,29 @@
 #include "targeting_reticle.h"
 
 #include "common.h"
+#include "panelfile.h"
+#include "panelquad.h"
+#include "trace.h"
 
 VALIDATE_SIZE(targeting_reticle, 0x1Cu);
+
+targeting_reticle::targeting_reticle()
+{
+    this->field_4 = 0;
+    this->field_8 = nullptr;
+    this->field_0 = false;
+}
+
+void targeting_reticle::init()
+{
+    if ( this->field_8 == nullptr )
+    {
+        this->field_8 = PanelFile::UnmashPanelFile("targeting_reticle", static_cast<panel_layer>(7));
+        this->field_C = this->field_8->GetPQ("TR_arrow_top_left");
+        this->field_10 = this->field_8->GetPQ("TR_arrow_top_right");
+        this->field_14 = this->field_8->GetPQ("TR_arrow_bottom_left");
+        this->field_18 = this->field_8->GetPQ("TR_arrow_bottom_right");
+        this->field_4 = 0;
+        this->field_0 = false;
+    }
+}
--- src/targeting_reticle.h
+++ src/targeting_reticle.h
@@ -1,5 +1,18 @@
 #pragma once
 
+struct PanelFile;
+struct PanelQuad;
+
 struct targeting_reticle {
-    char field_0[0x1C];
+    bool field_0;
+    int field_4;
+    PanelFile *field_8;
+    PanelQuad *field_C;
+    PanelQuad *field_10;
+    PanelQuad *field_14;
+    PanelQuad *field_18;
+
+    targeting_reticle();
+
+    void init();
 };
